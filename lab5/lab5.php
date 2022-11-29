774f4b798864182f6a16118feba8fb2a232f109f
Yurii Konovalov <mr.yuriikonovalov@gmail.com>
2022-11-29 22:21:16 +0200

    Lab5

---
+++ lab5/lab5.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="ua">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Лабораторна робота 5</title>
+  <link rel="stylesheet" href="../css/styles.css" />
+  <link rel="icon" href="../icon.png" />
+
+  <!-- Google Font -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Righteous:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+  <!-- Bootstrap Font Icon CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" />
+</head>
+
+<body class="body">
+  <nav id="navbar" class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand brand-title" href="../index.html">YK</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarToggler">
+      <ul class="navbar-nav ms-auto">
+        <il class="nav-item">
+          <a href="../lab2/lab2.php" class="nav-link">
+            Лабораторна робота <i class="bi bi-2-circle"></i>
+          </a>
+        </il>
+        <il class="nav-item">
+          <a href="../lab5/lab5.php" class="nav-link active">
+            Лабораторна робота <i class="bi bi-5-circle-fill"></i>
+          </a>
+        </il>
+        <il class="nav-item">
+          <a href="../lab8/lab8.php" class="nav-link">
+            Лабораторна робота <i class="bi bi-8-circle"></i>
+          </a>
+        </il>
+      </ul>
+    </div>
+  </nav>
+  <section class="p-5">
+    <?php
+    require "library.php";
+
+    $weather = request_weather('https://www.gismeteo.ua/ua/weather-kyiv-4944/');
+    echo "<p> $weather->city </p>";
+    echo "<p> $weather->date </p>";
+    echo "<br>";
+    echo "<p> Схід сонця: $weather->sunrise </p>";
+    echo "<p> Захід сонця: $weather->sunset </p>";
+    echo "<p> Тривалість дня: $weather->day_time </p>";
+    echo "<br>";
+    echo "<div>
+     <div class=\"temperature\"> Температура протягом дня:</div>";
+    foreach ($weather->temperatures as &$temp) {
+      echo "<div class=\"temperature\">$temp->hour г: $temp->degree &deg;</div>";
+    }
+    echo " </div> ";
+    ?>
+  </section>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ lab5/library.php
@@ -0,0 +1,159 @@
+<?php
+function request_weather($url = 'https://www.gismeteo.ua/ua/weather-kharkiv-5053/')
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    $data = curl_exec($curl);
+    curl_close($curl);
+    return new Weather($data);
+}
+
+class Temperature
+{
+    public string $hour;
+    public string $degree;
+
+    public function __construct(string $hour, string $degree) {
+        $this->hour = $hour;
+        $this->degree = $degree;
+    }
+}
+
+class Weather
+{
+    public string $city;
+    public string $date;
+    public string $sunrise;
+    public string $sunset;
+    public string $day_time;
+    public array $temperatures;
+
+    public function __construct(string $response)
+    {
+        $this->city = $this->get_city($response);
+        $this->date = $this->get_date($response);
+        $this->sunrise = $this->get_sunrise($response);
+        $this->sunset = $this->get_sunset($response);
+        $this->day_time = $this->get_day_time($response);
+        $this->temperatures = $this->get_temperatures($response);
+    }
+
+    private function  get_city($response)
+    {
+        $pattern = '/<div class=\"transparent-city js-transparent-city\">.*<\/div>/sU';
+        preg_match($pattern, $response, $matches);
+        $match = $matches[0];
+        $start = strlen("<div class=\"transparent-city js-transparent-city\">");
+        $end =  strlen($match) - $start - strlen("</div>");
+        return substr($match, $start, $end);
+    }
+
+    private function get_date($response)
+    {
+        $pattern = '/cacheDate: \'..\...\...../sU';
+        preg_match($pattern, $response, $matches);
+        $match = $matches[0];
+        $start = strlen("cacheDate: '");
+        $end =  strlen($match) - $start;
+        return substr($match, $start, $end);
+    }
+
+
+    private function get_sunrise($response)
+    {
+        $pattern = '/<div class="astro-sun">.*<div>Схід — .*:.*<\/div>/sU';
+        preg_match($pattern, $response, $matches);
+        $match = $matches[0];
+        $start_breakpoint = "Схід — ";
+        $start = strpos($match, $start_breakpoint) + strlen($start_breakpoint);
+        $end =  strlen($match) - $start - strlen("</div>");
+        return substr($match, $start, $end);
+    }
+
+    private function get_sunset($response)
+    {
+        $pattern = '/<div class="astro-sun">.*<div>Захід — .*:.*<\/div>/sU';
+        preg_match($pattern, $response, $matches);
+        $match = $matches[0];
+        $start_breakpoint = "Захід — ";
+        $start = strpos($match, $start_breakpoint) + strlen($start_breakpoint);
+        $end =  strlen($match) - $start - strlen("</div>");
+        return substr($match, $start, $end);
+    }
+
+
+    private function get_day_time($response)
+    {
+        $pattern = '/Тривалість дня: .*<\/div>/sU';
+        preg_match($pattern, $response, $matches);
+        $match = $matches[0];
+        $start = strlen("Тривалість дня: ");
+        $end =  strlen($match) - $start - strlen("</div>");
+        $day_time =  substr($match, $start, $end);
+        return $this->format_day_time($day_time);
+    }
+
+
+    private function format_day_time(string $day_time)
+    {
+        $array = explode(" ", $day_time);
+        $formatted_hours = $this->format_hours($array[0]);
+        // If more there's more than 2 items then the date also has a minute value.
+        if (count($array) > 2) {
+            return "$formatted_hours $array[2] $array[3]";
+        } else {
+            return "рівно $formatted_hours";
+        }
+    }
+
+    private function format_hours(string $hours)
+    {
+        return match ($hours) {
+            '1', '21' => "$hours година",
+            '2', '3', '4', '22', '23', '24' => "$hours години",
+            default =>  "$hours годин"
+        };
+    }
+
+    private function get_temperatures($response)
+    {
+        $pattern = '/width: 12.5%.*?<span class="unit unit_temperature_c">.*?<\/span>/';
+        preg_match_all($pattern, $response, $matches);
+        $match = $matches[0];
+        $temperatures = array();
+        for ($i = 0; $i < 8; $i++) {
+            $start_breakpoint = "<span class=\"unit unit_temperature_c\">";
+            $start = strpos($match[$i], $start_breakpoint) + strlen($start_breakpoint);
+            $end =  strlen($match[$i]) - $start - strlen("</span>");
+            $temperature =  substr($match[$i], $start, $end);
+            $temperature = str_replace('&minus;', '-', $temperature);
+            $temperatures[$i] = $temperature;
+        }
+
+        $hours = $this->get_temperature_hours($response);
+        $result = array();
+        for ($i = 0; $i < 8; $i++) {
+            $result[$i] = new Temperature($hours[$i], $temperatures[$i]);
+        }
+        return $result;
+    }
+
+    private function get_temperature_hours($response)
+    {
+        $pattern = '/<span>..?<sup class="time-sup">/';
+        preg_match_all($pattern, $response, $matches);
+        $match = $matches[0];
+        $hours = array();
+        for ($i = 0; $i < 8; $i++) {
+            $start = strlen("<span>");
+            $end =  strlen($match[$i]) - $start - strlen("<sup class=\"time-sup\">");
+            $hour =  substr($match[$i], $start, $end);
+            $hours[$i] = $hour;
+        }
+        return $hours;
+    }
+}
